5770fda44ffdfeccea9813d951727430d6dfd3d5
ahmfarisi <ahmfarisi@gmail.com>
2022-11-16 11:11:25 +0700

    Update ModelPahlawan.java

--- app/src/main/java/com/ahmfarisi/pahlawanku/ModelPahlawan.java
+++ app/src/main/java/com/ahmfarisi/pahlawanku/ModelPahlawan.java
@@ -1,4 +1,29 @@
 package com.ahmfarisi.pahlawanku;
 
 public class ModelPahlawan {
+    private String nama, tentang, foto;
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getTentang() {
+        return tentang;
+    }
+
+    public void setTentang(String tentang) {
+        this.tentang = tentang;
+    }
+
+    public String getFoto() {
+        return foto;
+    }
+
+    public void setFoto(String foto) {
+        this.foto = foto;
+    }
 }
