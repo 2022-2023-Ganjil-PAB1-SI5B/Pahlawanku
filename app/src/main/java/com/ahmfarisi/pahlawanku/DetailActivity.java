29535da701c8ba77e17551de8abd21a6f0bb7cbb
ahmfarisi <ahmfarisi@gmail.com>
2022-11-23 11:17:11 +0700

    Update DetailActivity.java

--- app/src/main/java/com/ahmfarisi/pahlawanku/DetailActivity.java
+++ app/src/main/java/com/ahmfarisi/pahlawanku/DetailActivity.java
@@ -2,13 +2,34 @@ package com.ahmfarisi.pahlawanku;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
 
 public class DetailActivity extends AppCompatActivity {
+    private ImageView ivFoto;
+    private TextView tvNama, tvTentang;
+    private String yNama, yTentang, yFoto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+        ivFoto = findViewById(R.id.iv_foto);
+        tvNama = findViewById(R.id.tv_nama);
+        tvTentang = findViewById(R.id.tv_tentang);
+
+        Intent terima = getIntent();
+        yNama = terima.getStringExtra("xNama");
+        yTentang = terima.getStringExtra("xTentang");
+        yFoto = terima.getStringExtra("xFoto");
+
+        tvNama.setText(yNama);
+        tvTentang.setText(yTentang);
+        Glide.with(DetailActivity.this).load(yFoto).into(ivFoto);
     }
 }
